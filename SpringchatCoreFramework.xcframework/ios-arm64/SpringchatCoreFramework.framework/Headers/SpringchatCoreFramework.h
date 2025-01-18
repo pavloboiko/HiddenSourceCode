44a5dc1ed9ca1e749fdacde8edc72828bd07f6e6
Pavlo Boiko <boiko.impul@gmail.com>
2025-01-18 05:14:32 -0500

    no message

--- SpringchatCoreFramework.xcframework/ios-arm64/SpringchatCoreFramework.framework/Headers/SpringchatCoreFramework.h
+++ SpringchatCoreFramework.xcframework/ios-arm64/SpringchatCoreFramework.framework/Headers/SpringchatCoreFramework.h
@@ -2,7 +2,7 @@
 //  SpringchatCoreFramework.h
 //  SpringchatCoreFramework
 //
-//  Created by Pavlo Boiko on 1/17/25.
+//  Created by Pavlo Boiko on 1/18/25.
 //
 
 #import <Foundation/Foundation.h>
